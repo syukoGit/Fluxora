6c3ac9e2cfeafac6978a1aace97c912d8fe00e5f
Antonin LYAËT <syukodev@outlook.com>
2025-11-10 14:09:55 +0100

    feat: add InputWithIcon component for enhanced input fields
    feat: create LoadingList component for displaying loading states in transaction lists
    
    feat: implement TransactionDialog component for adding and editing transactions
    
    feat: add TransactionListElement component to render individual transaction items
    
    feat: introduce ButtonGroup component for grouping buttons with consistent styling
    
    feat: create Calendar component for date selection with custom styling
    
    feat: implement Dialog components for modal functionality
    
    feat: add Empty components for displaying empty states in UI
    
    feat: create Form components for better form handling and validation
    
    feat: implement Popover component for contextual information display
    
    feat: add Select component for dropdown selections with custom styling
    
    feat: create Skeleton component for loading placeholders

---
+++ apps/web/src/components/budget/TransactionList/TransactionListElement.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import { CategoryDto, TransactionDto } from '@/lib/budget/transaction/types';
+import { formatDate } from '@/lib/utils';
+import TransactionDialog from './TransactionDialog';
+import { useState } from 'react';
+
+interface Props {
+  transaction: TransactionDto;
+  categories: CategoryDto[];
+  onTransactionUpdated?: () => void;
+}
+
+const TransactionListElement = ({ transaction, categories, onTransactionUpdated }: Props) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger className='w-full flex flex-row justify-between hover:bg-secondary rounded-md p-2 items-center cursor-pointer'>
+        <div className='flex flex-col items-start'>
+          <div className='text-base font-semibold'>{transaction.name}</div>
+          <div className='text-xs text-secondary-foreground font-thin text'>{formatDate(transaction.date)}</div>
+        </div>
+        <div className={`text-sm ${transaction.amount < 0 ? 'text-red-500' : 'text-green-500'} pr-6`}>
+          {transaction.amount.toFixed(2)} €
+        </div>
+      </DialogTrigger>
+      <TransactionDialog
+        transaction={transaction}
+        categories={categories}
+        open={open}
+        setOpen={setOpen}
+        {...(onTransactionUpdated && { onSuccess: onTransactionUpdated })}
+      />
+    </Dialog>
+  );
+};
+
+export default TransactionListElement;
